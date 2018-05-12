eb49b31b27902983aa3f572a9dfa1fe757064e3b
vertusd <vertusd@gmail.com>
2018-05-12 22:54:08 +0800

    增加STRUT服务端代码，增加中文，增加对象开始结束时间属性，

---
+++ Strut/app/bundles/app/strut.deck/Component.js
@@ -0,0 +1,70 @@
+/**
+ * @author Matt Crinklaw-Vogt
+ */
+define(["./SpatialObject"], function(SpatialObject) {
+	var defaultScale, defaults;
+	defaults = {
+		x: config.slide.size.width / 3,
+		y: config.slide.size.height / 3
+	};
+
+	defaultScale = {
+		x: 1,
+		y: 1
+	};
+
+	/**
+	 * Base class for all slide elements.
+	 *
+	 * @class Component
+	 * @augments SpatialObject
+	 */
+	return SpatialObject.extend({
+
+		/**
+		 * Initialize component model.
+		 * @returns {Object}
+		 */
+		initialize: function() {
+			_.defaults(this.attributes, defaults);
+			if (this.attributes.scale === undefined) {
+				this.attributes.scale = {};
+				return _.defaults(this.attributes.scale, defaultScale);
+			}
+			if (this.attributes.startTime === undefined)
+			{
+				this.attributes.startTime = 0;
+			}
+			if (this.attributes.endTime === undefined)
+			{
+				this.attributes.endTime = 0;
+			}
+		},
+
+		/**
+		 * Sets or returns custom classes of the element.
+		 *
+		 * @param {string} [classes] If passed, element will take these classes.
+		 * @returns {string}
+		 */
+		customClasses: function(classes) {
+			if (classes == null) {
+				return this.get('customClasses');
+			} else {
+				this.set('customClasses', classes);
+			}
+		},
+		
+		/**
+		 * Dispose the element.
+		 */
+		dispose: function() {
+			this.trigger("dispose", this);
+			this.off();
+		},
+
+		constructor: function Component() {
+			SpatialObject.prototype.constructor.apply(this, arguments);
+		}
+	});
+});
